5db8a40d2bdf675ae525acb307ae7b9e99074ae0
Ugonna Ojiaku CW <a8hf1zz@mmm.com>
2018-09-13 17:20:13 -0500

    Importing Techniques

--- liri.js
+++ liri.js
@@ -3,3 +3,9 @@ require("dotenv").config();
 // Add the code required to import the keys.js file and store it in a variable (?)
 
 var spotify = new Spotify(keys.spotify);
+
+var add = (function(exports) {
+    return exports;
+})
+
+import './keys.js';
